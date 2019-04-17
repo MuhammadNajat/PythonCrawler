3cf0fd7ea59691bcae72ea1096ba853dfb6f2e81
MuhammadNajat <17946910+MuhammadNajat@users.noreply.github.com>
2019-04-17 21:53:59 +0600

    Update crawler_contest.py

--- crawler_contest.py
+++ crawler_contest.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
         url = basicUrl + str(pageId)
         content = requests.get(url)
         text = unescape(content.text)
-        if not content.ok:
-            break
+        
         #matcher = re.compile(r'<td>(\d*)</td><td><a href="/users/icpc_dhaka_18_\d*?">(BUET.*?) \[ Bangladesh University of Engineering and Technology \]</a></td><td><a href="/contests/icpc-dhaka-18/submissions/for\?user=icpc_dhaka_18_.*?"><div class="label label-info">(.*?)</div><div class="label label-default">(.*?)</div></a><td style="width: 24px;"></td></td><td><a href="/contests/icpc-dhaka-18/submissions/')
         matcher = re.compile(r'<td>(\d*)</td><td><a href="/users/icpc_dhaka_18_\d*?">(SUST.*?)\[ .*? \]</a></td><td><a href="/contests/icpc-dhaka-18/submissions/for\?user=icpc_dhaka_18_\d*?"><div class="label label-info">(\d*?)</div><div class="label label-default">(\d*?)</div></a><td style="width: 24px;"></td></td><td><a href="/contests/icpc-dhaka-18/submissions/', re.IGNORECASE)
 
